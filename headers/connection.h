f1686ffa7b0202cbae23a85fe2a5503ceb9c32d0
f0fx <luka2317@its.edu.rs>
2024-06-30 12:16:47 +0200

    [+] Added the ability to enter your desired port and backlog

--- headers/connection.h
+++ headers/connection.h
@@ -4,7 +4,7 @@
 using nlohmann::json;
 
 namespace Server{
-    void Listen(short (*HandlePost)(int clientSocket, json requestJSON,std::string requestRoute),short (*HandleGet)(int clientSocket, std::string requestRoute));
+    void Listen(int port,short (*HandlePost)(int clientSocket, json requestJSON,std::string requestRoute),short (*HandleGet)(int clientSocket, std::string requestRoute),int backlog = 1313);
     // Route, Type, Need to pass 
     short HandleRequest(int clientSocket,short (*HandlePost)(int clientSocket, json requestJSON, std::string requestRoute),short (*HandleGet)(int clientSocket, std::string requestRoute));
 }
