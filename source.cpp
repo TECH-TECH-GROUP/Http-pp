050271e1c52118a3228339a54fd5c82643ce8253
f0fx <luka2317@its.edu.rs>
2024-07-03 12:02:19 +0200

    [^] Fixed source.cpp example

--- source.cpp
+++ source.cpp
@@ -3,22 +3,23 @@
 #include "headers/connection.h"
 
 // use this file for testing
-short HandlePost(int clientSocket, json requestJSON,std::string requestRoute,json headers,std::string apiVersion="");
-short HandleGet(int clientSocket, std::string requestRoute,json headers,std::string apiVersion="");
+short HandlePost(int clientSocket, json requestJSON,std::string requestRoute,json headers,std::string apiVersion);
+short HandleGet(int clientSocket, std::string requestRoute,json headers,std::string apiVersion);
 
 int main(){
     Server::Listen(1313,HandlePost,HandleGet);
     return 0;
 }
 
-short HandlePost(int clientSocket, json requestJSON,std::string requestRoute,json headers){
+short HandlePost(int clientSocket, json requestJSON,std::string requestRoute,json headers,std::string apiVersion){
     json responseJSON = {};
     Response::RespondJSON(clientSocket,0,responseJSON);
+    printf("%s\n",apiVersion.c_str());
 
     return 0;
 }
 
-short HandleGet(int clientSocket, std::string requestRoute,json headers){
+short HandleGet(int clientSocket, std::string requestRoute,json headers,std::string apiVersion){
     json responseJSON = {};
     Response::RespondJSON(clientSocket,0,responseJSON);
 
